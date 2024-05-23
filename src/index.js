517b6f0d0aa8fd1c04dfc0329aea2994e5dd8a56
ANDRE LUIZ ARANTES <andre_arantes@estudante.sesisenai.org.br>
2024-05-22 22:15:39 -0300

    Adicionando conexao com MySql

--- src/index.js
+++ src/index.js
@@ -1,80 +1,18 @@
-const express = require("express");
+import express from 'express';
+import UsuariosController from './controllers/UsuariosController.js';
+
 const port = 3000;
 
 const app = express();
 app.use(express.json());
 
-let usuarios = [];
+const usuariosController = new UsuariosController();
 
-//GET
-app.get("/usuarios", (req, res) => {
-  // console.log(req.query);
-  let resultado = usuarios;
-  // faz o filtro se receber ?filtro=exemplo
-  if (req.query.filtro) {
-    resultado = usuarios.filter((u) => {
-      return u.nome.includes(req.query.filtro);
-    });
-  }
-  res.send(resultado);
-});
-
-//POST
-app.post("/usuarios", (req, res) => {
-  console.log(req.body);
-
-  if (!req.body || !req.body.nome || !req.body.email) {
-    res.status(400).send('Os campos nome e email são obrigatórios.');
-    return;
-  }
-
-  const usuarioJaExiste = usuarios.find((usu) => usu.email === req.body.email);
-
-  if (usuarioJaExiste) {
-    res.status(409).send('Usuário já existe!');
-    return;
-
-  }
-
-  // res.send(`Chamou o POST com o nome: ${req.body.nome}`);
-
-  //gerando id aleatorio de exemplo
-  const novoUsuario = { ...req.body, id: +new Date() };
-
-  usuarios.push(novoUsuario);
-  res.status(201).send(novoUsuario);
-});
-
-//PUT
-app.put("/usuarios", (req, res) => {
-  // console.log(req.headers);
-  usuarios = usuarios.map((user) => {
-    if (user.id === req.body.id) {
-      return req.body;
-    } else {
-      return user;
-
-    }
-  });
-
-  res.send('Operação efetuada com sucesso!')
-
-  // if (!req.headers.autorizacao) {
-  //   res.status(401).send('Informe o HEADER "autorizacao"');
-  // } else {
-  //   res.send("Chamou o PUT!");
-  // }
-});
-
-//DELETE
-app.delete("/usuarios/:idUsuario", (req, res) => {
-  // console.log(req.params);
-  usuarios = usuarios.filter((user) => {
-    return user.id !== +req.params.idUsuario;
-  })
-
-  res.send("Operação efetuada com sucesso!");
-});
+//CRUD USUARIOS
+app.get('/usuarios', usuariosController.listar);
+app.post('/usuarios', usuariosController.adicionar);
+app.put('/usuarios', usuariosController.atualizar);
+app.delete('/usuarios/:idUsuario', usuariosController.excluir);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
